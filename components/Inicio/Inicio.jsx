aeeff612945747b3e0e3576d7f77652300c9a05b
Carlos Parra <86031209+ChayOne@users.noreply.github.com>
2022-04-09 23:11:12 -0500

    First

---
+++ components/Inicio/Inicio.jsx
@@ -0,0 +1,12 @@
+import 'animate.css'
+
+export default function Inicio() {
+    return (
+        <div className="container-index">
+            <div className='animate__animated animate__fadeInRight'>
+                <h1 className='index-title' >CARLOS <span>PARRA</span></h1>
+                <h4 className='index-subtitle' >DISEÑO GRÁFICO / WEB / UX</h4>
+            </div>
+        </div>
+    )
+}
