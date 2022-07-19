d7819c6663a066fdcd7263d99153c6358e9e7006
Carlos Parra <86031209+CarlosParraMtz@users.noreply.github.com>
2022-07-18 19:53:20 -0500

    a

--- components/Inicio/Inicio.jsx
+++ components/Inicio/Inicio.jsx
@@ -1,14 +1,23 @@
+import { useState } from 'react'
 import 'animate.css'
 import Menu from '../Menu'
+import { Dialog } from '@mui/material'
 
 
 export default function Inicio() {
+
+    const [pag, setPag] = useState(null)
 
     return (
         <div className="container-index">
             <h1 className='index-title' >CARLOS <span>PARRA</span></h1>
             <h4 className='index-subtitle animate__animated animate__bounceIn' >DISEÑO WEB / UX</h4>
-            <Menu/>
+            <Menu setPag={setPag} />
+
+            <Dialog open={pag !== null} fullWidth >
+                Prueba
+            </Dialog>
+
         </div>
     )
 }
--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,27 +1,27 @@
 import { useRouter } from "next/router"
 
-export default function Menu() {
+export default function Menu({ setPag }) {
 
     const Router = useRouter();
 
     return (
         <div className='index-menu' >
-            <button className="index-menu-item" onClick={()=>Router.push('/work#about')} >
+            <button className="index-menu-item" onClick={() => setPag('about')} >
                 <img src='/images/icons/programmer.png' width='50px' alt='Sobre mí' />
                 <p>Sobre mí</p>
             </button>
-            <button className="index-menu-item" onClick={()=>Router.push('/work#skills')} >
+            <button className="index-menu-item" onClick={() => Router.push('/work#skills')} >
                 <img src='/images/icons/technology.png' width='50px' alt='Sobre mí' />
                 <p>Tecnologías</p>
             </button>
-            
-            
+
+
             <button className="index-menu-item">
                 <img src='/images/icons/portfolio.png' width='50px' alt='Sobre mí' />
                 <p>Portafolios</p>
-            </button> 
-            
-            <button className="index-menu-item" onClick={()=>Router.push('/work')} >
+            </button>
+
+            <button className="index-menu-item" onClick={() => Router.push('/work')} >
                 <img src='/images/icons/comunicar.png' width='50px' alt='Sobre mí' />
                 <p>Comunicarse conmigo</p>
             </button>
