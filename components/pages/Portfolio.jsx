290562aded51daf3f4827f0c38d0969ac35df553
CarlosParraMtz <chay.exe@outlook.com>
2022-11-03 09:27:22 -0600

    Cambios en tamaños de pantalla

--- components/pages/Portfolio.jsx
+++ components/pages/Portfolio.jsx
@@ -1,10 +1,21 @@
+import ButtonRight from "../elements/ButtonRight"
+import ButtonLeft from "../elements/ButtonLeft"
+
 export default function Portfolio() {
     return (
         <section id='portfolio'>
-            <h2>Portafolios</h2>
-            <div className="container-content">
-                p
+            <div className="centered">
+
+                <h1>Web developer portfolio</h1>
+                <div className="container-content">
+                    p
+                </div>
             </div>
+
+
+            <ButtonLeft link="/" title="Home" />
+            <ButtonRight link="/contact" title="Contact" />
+
         </section>
     )
 }
