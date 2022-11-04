5ed2471986fbb8cc8d7eeed2fa1f7e3812ef4457
Carlos Parra <86031209+CarlosParraMtz@users.noreply.github.com>
2022-11-03 23:14:48 -0600

    Update to portfolio

---
+++ components/elements/Chip.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Chip({ label }) {
+    return (
+        <div className='chip' >
+            <p><b> {label} </b></p>
+        </div>
+    )
+}
--- components/pages/Portfolio.jsx
+++ components/pages/Portfolio.jsx
@@ -1,15 +1,26 @@
-import ButtonRight from "../elements/ButtonRight"
-import ButtonLeft from "../elements/ButtonLeft"
+import Chip from '../elements/Chip';
 
+import ButtonRight from "../elements/ButtonRight";
+import ButtonLeft from "../elements/ButtonLeft";
+
+/* 
+<div className="chips">
+<Chip label="react" />
+<Chip label="next" />
+<Chip label="firebase" />
+<Chip label="react" />
+</div>
+*/
 export default function Portfolio() {
     return (
         <section id='portfolio'>
             <div className="centered">
-
                 <h1>Web developer portfolio</h1>
-                <div className="container-content">
-                    p
-                </div>
+
+                <p>
+                    Showing all my finished projects and the ones I'm working on.
+                </p>
+
             </div>
 
 
