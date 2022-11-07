bef0f82fbc312d9bd61eaecd8daf70f752c2e82b
Carlos Parra <86031209+CarlosParraMtz@users.noreply.github.com>
2022-11-06 21:11:05 -0600

    Update Portfolio page

--- components/pages/PortfolioPost.jsx
+++ components/pages/PortfolioPost.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SideButton from '../elements/SideButton';
 import ReactMarkdown from 'react-markdown';
 import Image from "next/image";
+import Chip from '../elements/Chip';
 
 export default function PortfolioPost({ data }) {
     return (
@@ -9,6 +10,11 @@ export default function PortfolioPost({ data }) {
             <div className="centered">
 
                 <h1> {data.projectName} </h1>
+                <div className="chips-container">
+                    {
+                        data.skills.map((skill, index) => <Chip key={index} label={skill} />)
+                    }
+                </div>
                 <div className="cover-img-container">
                     <Image src={`/portfolio/${data.thumbnail}`} layout="fill" alt={data.projectName} />
                 </div>
