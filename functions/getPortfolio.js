4664238db56bdc98eeb98297343e14dab25d68aa
Carlos Parra <carlosparramtz@hotmail.com>
2023-10-18 16:28:49 -0600

    redesign started

--- functions/getPortfolio.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-export default async function getPortfolio() {
-    return await axios.get(`https://cdn.contentful.com/spaces/${process.env.CF_PORTFOLIO_SPACE}/entries?access_token=${process.env.CF_PORTFOLIO_ACCESSTOKEN}`)
-        .then(res => (res.data.items.map(item => item.fields)));
-}
--- pages/index.js
+++
@@ -1,2 +0,0 @@
-import Inicio from '../components/pages/Inicio'
-export default function Home() { return <Inicio /> }
